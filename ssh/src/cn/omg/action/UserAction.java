6e4a0ab4475fe14845139d540a4a017bee8519af
richardNing <richard.ning@gmail.com>
2017-10-26 16:33:27 +0800

    1

---
+++ ssh/src/cn/omg/action/UserAction.java
@@ -0,0 +1,13 @@
+package cn.omg.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport {
+
+		@Override
+		public String execute() throws Exception {
+			// TODO Auto-generated method stub
+			System.out.println("userAction...............");
+			return NONE;
+		}
+}
